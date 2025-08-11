180be9820393a49a48d646a1faaefe0dadb38262
경원 <kyungwon.park0407@gmail.com>
2025-08-11 11:51:13 +0900

    TestUtillTest t1 red

---
+++ src/test/java/TestUtillTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class TestUtillTest {
+    @Test
+    @DisplayName("TestUtill.genScanner()")
+    void t1() {
+        //*테스트트 도구를 만들 떄도 TDD방식을 사용해도 된다*//
+        Scanner sc = TestUtill.genScanner("""
+                        등록
+                        너 자신을 알라
+                        소크라테스"""
+        );
+
+        String cmd = sc.nextLine();
+        String saying = sc.nextLine();
+        String author = sc.nextLine();
+
+        assertThat(cmd).isEqualTo("등록");
+        assertThat(saying).isEqualTo("너 자신을 알라");
+        assertThat(author).isEqualTo("소크라테스");
+
+
+        assertThat(out)
+                .contains("명언 :")
+                .contains("작가 :")
+                .contains("1번 명언이 등록되었습니다.");
+    }
+}
