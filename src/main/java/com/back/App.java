40a069977b113f26e0cc6ebb6b44b866d231e550
경원 <kyungwon.park0407@gmail.com>
2025-08-11 14:53:44 +0900

    step5 - green & blue

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -1,5 +1,7 @@
 package com.back;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -14,6 +16,7 @@ public class App {
         System.out.println("== 명언 앱 ==");
 
         int lastId= 0;
+        List<WiseSaying> wiseSayings = new ArrayList<>();
 
         while (true) {
             System.out.print("명령) ");
@@ -27,9 +30,22 @@ public class App {
                     String author = sc.nextLine();
 
                     lastId++;
+                    WiseSaying wiseSaying = new WiseSaying(lastId, saying, author);
+                    wiseSayings.add(wiseSaying);
 
                     System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
                     System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+                }
+                case "목록" -> {
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------\n");
+
+
+                    wiseSayings
+                            .reversed()
+                            .stream()
+                            .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n",
+                                    wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getSaying()));
                 }
                 case "종료" -> {
                     return;
