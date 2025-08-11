472c0aee7b886bfc153e5b2bf0f486052cb0397e
경원 <kyungwon.park0407@gmail.com>
2025-08-11 14:16:01 +0900

    AppTest t2 red&green

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -12,6 +12,20 @@ public class App {
 
     void run() {
         System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = sc.nextLine(); // 종료가 안되면 돌아와서 입력값이 꼬일 수 있음
+
+            if (cmd.equals("등록")) {
+                System.out.print("명언: ");
+                String saying = sc.nextLine();
+                System.out.print("작가: ");
+                String author = sc.nextLine();
+            } else if (cmd.equals("종료")){
+                break;
+            }
+        }
     }
 
 
