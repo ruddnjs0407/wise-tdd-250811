e76aaf62321ea1a38e2be83b2a8b5b9c2c403e39
경원 <kyungwon.park0407@gmail.com>
2025-08-11 12:14:30 +0900

    ㅆTestUtillTest t2 red

--- src/test/java/TestUtillTest.java
+++ src/test/java/TestUtillTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,11 +25,22 @@ public class TestUtillTest {
         assertThat(cmd).isEqualTo("등록");
         assertThat(saying).isEqualTo("너 자신을 알라");
         assertThat(author).isEqualTo("소크라테스");
+    }
 
+    @Test
+    @DisplayName("TestUtil.setOutToByteArray()")
+    void t2() {
 
-//        assertThat(out)
-//                .contains("명언 :")
-//                .contains("작가 :")
-//                .contains("1번 명언이 등록되었습니다.");
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
+
+        System.out.println("1 / 이순신 / 나의 죽음을 적에게 알리지 마라");
+
+        String outStr = outputStream.toString();
+
+        TestUtil.clearSetOutToByteArray(); //원상복구
+
+        assertThat(outStr).isEqualTo("1 / 이순신 / 나의 죽음을 적에게 알리지 마라");
+
+        System.out.println("이제 화면에 출력됩니다.");
     }
 }
